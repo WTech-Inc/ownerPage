ef9f415a58be93e196889878aeefa6e2b1382273
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-07-23 01:41:46 +0000

    加入了/donate 路由

--- src/main/java/com/wangtry/webserver/controller/Controller.java
+++ src/main/java/com/wangtry/webserver/controller/Controller.java
@@ -2,6 +2,10 @@ package com.wangtry.webserver.controller;
 
 import com.wangtry.webserver.service.*;
 import org.springframework.web.bind.annotation.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.*;
+import org.json.*;
 
 @RestController
 public class Controller {
@@ -18,5 +22,24 @@ public class Controller {
     @GetMapping("/get-info")
     public Object printInfo() {
         return profile.getInfo();
+    }
+    @GetMapping("/donate")
+    public String payWbank() {
+        String reqUrl = profile.getWbankCardUrl();
+        JSONObject json = new JSONObject(profile.getWbankObject());
+        URL url = new URL(reqUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("PATCH");
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Content-Type", "application/json");
+        OutputStream os = conn.getOutputStream();
+        os.write(json.toString().getBytes());
+        
+        int statusCode = conn.getResponseCode();
+        if (statusCode == 200) {
+            return "success";
+        } else {
+            return "Not success";
+        }
     }
 }
--- src/main/java/com/wangtry/webserver/service/ownerProfile.java
+++ src/main/java/com/wangtry/webserver/service/ownerProfile.java
@@ -6,6 +6,7 @@ import java.util.*;
 @Service
 public class ownerProfile {
     public String getName() { return "wangtry"; }
+    public String getWbankCardUrl() { return "https://wtechhk.com/wbank/card/action"; }
     public Object getInfo() {
         Map <String, Object> info = new HashMap<>();
         String[] hobbies = { "gym", "making-video", "cutting-video", "coding"};
@@ -14,5 +15,14 @@ public class ownerProfile {
         info.put("career", "student");
         info.put("hobbies", hobbies);
         return info;
+    }
+    public Object getWbankObject() {
+        Map <String, String> wbankInfo = new HashMap<>();
+        wbankInfo.put("cardNumber", "136ac5102067b800ec5d645e26e80074f44e54b560a3481c856aea0d694c6c49");
+        wbankInfo.put("password", "Chan1234!");
+        wbankInfo.put("accessKey", "0118");
+        wbankInfo.put("reviewer", "wangtry");
+        wbankInfo.put("amount", "1000");
+        return wbankInfo;
     }
 }
